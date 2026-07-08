a028f6599011d6fe1eb1d8760acfd5ad19b0dc6a
Jean Mc Bean <jeanjdm1997@gmail.com>
2026-07-08 17:15:04 +0200

    save

--- Labfiles/07-agent-framework/python/agent-framework.py
+++ Labfiles/07-agent-framework/python/agent-framework.py
@@ -4,6 +4,11 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 # Add references
+from agent_framework import tool, Agent
+from agent_framework.foundry import FoundryChatClient
+from azure.identity import AzureCliCredential
+from pydantic import Field
+from typing import Annotated
 
 
 load_dotenv()
@@ -27,9 +32,24 @@ async def main():
 async def process_expenses_data(prompt, expenses_data):
 
     # Create a foundry chat client
-    
+    client = FoundryChatClient(
+        project_endpoint=os.environ.get("PROJECT_ENDPOINT"),
+        model=os.getenv("MODEL_DEPLOYMENT_NAME"),
+        credential=AzureCliCredential()
+    )
 
     # Initialize an agent with the tool and instructions
+    async with (
+        Agent(
+            client=client,
+            name="Expense_Claims_Agent",
+            instructions="You are an agent that processes expense claims. You will receive a prompt and the expenses data, and you will use the submit_claim tool to send an email with the processed information." \
+            "Then confirm to the user that you've done so. Don't ask for any more information from the user, just use the data provided to create the email."
+            " At the user's request, create an expense claim and use the plug-in function to send an email to expenses@contoso.com with the subject 'Expense Claim`and a body that contains itemized expenses with a total."
+            tools=[submit_claim],
+        ) as agent,
+
+        ):
     
 
         # Use the agent to process the expenses data    
@@ -37,6 +57,14 @@ async def process_expenses_data(prompt, expenses_data):
 
 
 # Create a tool function for the email functionality
+@tool(approval_mode="never_required")
+def submit_claim(
+    to: Annotated[str, Field(description="Who to send the email to")],
+    subject: Annotated[str, Field(description="The subject of the email")],
+    body: Annotated[str, Field(description="The body of the email")]):
+    print ("\nTo:", to)
+    print ("Subject:", subject)
+    print (body, "\n")
 
 
 
